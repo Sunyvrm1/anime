a11ab24ab861034b0ea36112f3521b3b466a618a
Sushil Verma <115399549+Sunyvrm1@users.noreply.github.com>
2024-01-13 07:52:48 +0000

    nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,23 +14,40 @@ const gotoSlide = function (gotoSlide) {
   });
 };
 
-const next = function(){
-    if (curSlide == maxSlide - 1) {
-        curSlide = 0;
-      } else {
-        curSlide++;
-      }
-      gotoSlide(curSlide);
-}
+const next = function () {
+  if (curSlide == maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  gotoSlide(curSlide);
+};
 
-const prev = function(){
-    if (curSlide == 0) {
-        curSlide = maxSlide - 1;
-      } else {
-        curSlide--;
-      }
-      gotoSlide(curSlide);
-}
+const prev = function () {
+  if (curSlide == 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  gotoSlide(curSlide);
+};
 
 right.addEventListener("click", next);
 left.addEventListener("click", prev);
+
+//Navigation
+
+const navIcon = document.querySelector(".navIcon");
+const sideNavigation = document.querySelector(".sideNavigation");
+const closeBtn = document.querySelector(".closeBtn");
+const nav = document.querySelector("nav");
+
+navIcon.addEventListener("click", () => {
+  sideNavigation.classList.toggle("activeNav");
+  nav.classList.toggle("activeMenu");
+});
+
+closeBtn.addEventListener("click", () => {
+  sideNavigation.classList.toggle("activeNav");
+  nav.classList.toggle("activeMenu");
+});
